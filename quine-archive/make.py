14a7fded10a5b5b84e57805365ebcdef273c0911
Matthew Barber <quitesimplymatt@gmail.com>
2018-05-02 20:52:15 +0100

    standardized make.py for future implementations of different compression methods

--- quine-archive/make.py
+++ quine-archive/make.py
@@ -1,36 +1,36 @@
-import binascii
 from datetime import datetime
 import calendar
 
-MAGIC_NUM = 0x1f8b
-COMPRESSION_METHOD = 0x08 # defalte
-FILE_FLAGS = 0x08 # accepts filename as extra field
-COMPRESSION_FLAGS = 0x00
-OS_TYPE = 0xff # unknown
+def gz(filename = 'quine'):
+    ### Create header data
+    ## Static member data
+    magic_num = 0x1f8b
+    compression_method = 0x08 # defalte
+    file_flags = 0x08 # accepts filename as extra field
+    compression_flags = 0x00
+    os_type = 0xff # unknown
 
-def gz(filename = 'q'):
-    ### Creating header data
     ## Find current time for date modified field
     current_time = datetime.utcnow()
-    unix_time = calendar.timegm(current_time.utctimetuple()) # integer representation
+    unix_time = calendar.timegm(current_time.utctimetuple())
 
     ## Concat members together by byte
     # Split members that are over a byte
-    magic_num_bin = MAGIC_NUM.to_bytes(2, byteorder='big')
+    magic_num_bin = magic_num.to_bytes(2, byteorder='big')
     unix_time_bin = unix_time.to_bytes(4, byteorder='little')
 
     # Initialize standard header format
     head = [
         magic_num_bin[0],
         magic_num_bin[1],
-        COMPRESSION_METHOD,
-        FILE_FLAGS,
+        compression_method,
+        file_flags,
         unix_time_bin[0],
         unix_time_bin[1],
         unix_time_bin[2],
         unix_time_bin[3],
-        COMPRESSION_FLAGS,
-        OS_TYPE
+        compression_flags,
+        os_type
     ]
 
     # Append filename data
@@ -38,8 +38,9 @@ def gz(filename = 'q'):
         head.append(byte)
     head.append(0x00) # zero terminate filename field
 
-    print(head)
+    ## Create header
+    head_bytes = bytearray(head)
 
-    return head
+    return head_bytes
 
-gz()
+print(gz())
